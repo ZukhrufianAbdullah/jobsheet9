fbd208b072f7bcd2a56ee19204f6b13451d59c29
ZukhrufianAbdullah <zukhrufianabdullah@gmail.com>
2024-10-28 09:22:05 +0700

    Add files via upload

---
+++ PolaPersegi326.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PolaPersegi326{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N (minimal 3): ");
+        int N = sc.nextInt();
+
+        //Validasi input agar minimal 3
+        if (N < 3) {
+            System.out.println("Nilai N harus minimal 3.");
+            return;
+        }
+
+        //Cetak persegi angka dengan bagian tengah kosong
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                //Kondisi untuk mencetak angka di bagian pinggir
+                if (i == 0 || i == N - 1 || j == 0 || j == N - 1) {
+                    System.out.print(N + " ");
+                } else{
+                    System.out.print("  "); //Bagian tengah kosong
+                }
+            }
+            System.out.println(); //Pindah baris
+        }
+    }
+}
---
+++ Tugas226.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class Tugas226 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Deklrasi variabel
+        String nama, namaKampus, jenisOlahraga = "";
+        String daftarAtletBadminton = "";
+        String daftarAtletTenisMeja = "";
+        String daftarAtletVoli = "";
+        String daftarAtletBasket = "";
+
+        //Perulangan paling luar
+        do {
+            System.out.print("Masukkan nama Politeknik (Ketik 'batal' untuk berhenti): ");
+            namaKampus = sc.nextLine();
+
+            //Menghentikan program jika input 'batal'
+            if (namaKampus.equalsIgnoreCase("batal")) {
+                System.out.println("Program dihentikan");
+                break;
+            }
+
+            //Perulangan kedua
+            for (int jnisOlahraga = 1; jnisOlahraga <= 4; jnisOlahraga++){
+                //Menyimpan nama atlet dalam string yang sesuai
+                switch (jnisOlahraga) {
+                    case 1:
+                        jenisOlahraga = "Badminton";
+                        break;
+                    case 2:
+                        jenisOlahraga = "Tenis Meja";
+                        break;
+                    case 3:
+                        jenisOlahraga = "Voli";
+                        break;
+                    case 4:
+                        jenisOlahraga = "Basket";
+                        break;
+                }
+                System.out.println("Input nama atlet " + jenisOlahraga);
+
+                //Perulangan terdalam
+                for (int j = 1; j <= 5; j++){
+                    System.out.print("Masukkan nama atlet ke " + j + ": ");
+                    nama = sc.nextLine();
+                    //Menyimpan jenis olahraga dalam string yang sesuai
+                    switch (jnisOlahraga){
+                        case 1:
+                            daftarAtletBadminton += nama + ", ";
+                            break;
+                        case 2:
+                            daftarAtletTenisMeja += nama + ", ";
+                            break;
+                        case 3:
+                            daftarAtletVoli += nama + ", ";
+                            break;
+                        case 4:
+                            daftarAtletBasket += nama + ", ";
+                            break;
+                    }
+                }
+            }
+            //Output daftar nama atlet per kampus yang dikelompokkan berdasarkan jenis olahraga
+            System.out.println("\nDaftar nama atlet " + namaKampus + ":");
+            System.out.println("Badminton: " + daftarAtletBadminton);
+            System.out.println("Tenis Meja: " + daftarAtletTenisMeja);
+            System.out.println("Voli: " + daftarAtletVoli);
+            System.out.println("Basket: " + daftarAtletBasket);
+            System.out.println();
+        }while(true);
+    }
+}
